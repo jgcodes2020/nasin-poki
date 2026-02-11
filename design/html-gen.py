7b3c456418217148654063e2995a86abef6267e2
jgcodes2020 <j349guo@uwaterloo.ca>
2026-02-11 16:07:41 -0500

    first commit

---
+++ design/html-gen.py
@@ -0,0 +1,48 @@
+
+SP_MAPPING = {}
+COLOR_MAPPING = {}
+
+with open("csp-words.txt") as file:
+    for line in file:
+        [num, word] = line.strip().split("  ")
+        num = int(num, base=16)
+        SP_MAPPING[num] = word.lower()
+
+def load_categories(filename):
+    global COLOR_MAPPING
+    with open(filename) as file:
+        for line in file:
+            [angle, *words] = line.strip().split(" ")
+            for word in words:
+                if word in COLOR_MAPPING:
+                    raise ValueError(f"word {word} cannot be assigned to angle {angle} (has angle {COLOR_MAPPING[word]})")
+                COLOR_MAPPING[word] = angle
+            
+            print(f"{angle:>3}, {len(words)}")
+
+load_categories("categories.txt")
+
+with open("output.html", "w") as file:
+    file.write("""\
+<html>
+<head>
+<title>sitelen nasa a</title>
+<link rel="stylesheet" href="test.css">
+</head>
+""")
+    file.write("<body>\n<table>\n")
+    for row in range(0xF1900, 0xF1A00, 0x010):
+        file.write("<tr>")
+        file.write(f"<th>U+{row:X}</th>")
+        for col in range(0, 16):
+            cp = row + col
+            lookup = SP_MAPPING.get(cp) or ""
+            angle = COLOR_MAPPING.get(lookup)
+            angle = f" style=\"background-color: hsl({angle} 100% 75%);\"" if angle is not None else ""
+            file.write(f"<th class=\"sitelen-pona\"{angle}>{lookup}</th>")
+            pass
+    
+        file.write("</tr>\n")
+        pass
+    file.write("</table>\n</body>\n")
+    file.write("</html>\n")
