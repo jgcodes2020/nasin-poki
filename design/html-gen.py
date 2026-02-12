093d45945639348361f5ba578d0bd669bf846dc3
jgcodes2020 <j349guo@uwaterloo.ca>
2026-02-11 21:07:56 -0500

    i might settle on this for now

--- design/html-gen.py
+++ design/html-gen.py
@@ -22,7 +22,7 @@ def load_categories(filename):
             
             print(f"{angle:>3}: {len(words):<2} | {'*' * (len(words) * 2)}")
 
-load_words("wordlist-2022.txt")
+load_words("wordlist-spk1.txt")
 load_categories("categories.txt")
 
 with open("output.html", "w") as file:
